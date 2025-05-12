a8716a7a68bf4c4a4f6fe6b97bfa5eac066c9bc2
nulller-python <test.null.2024@gmail.com>
2025-05-12 19:12:31 +0530

    pull

---
+++ gitpuller.py
@@ -0,0 +1,42 @@
+import flet as ft
+import os
+
+def main(page: ft.Page):
+    page.title = "Commiter"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+    # UI Components
+    text = ft.Text(
+        "GET THE LATEST UPDATES FROM SERVER",
+        size=15,
+        weight=ft.FontWeight.BOLD
+    )
+    button = ft.IconButton(
+        icon=ft.icons.CHECK_CIRCLE,
+        icon_size=40,
+        on_click=git_caller,
+        tooltip="Commit changes"
+    )
+    
+    # Layout
+    page.add(
+        ft.Column(
+            [
+                text,
+                
+                ft.Row([button], alignment=ft.MainAxisAlignment.CENTER)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        )
+    )
+
+def git_caller(prompt):
+    # Change to your project directory if needed
+    # os.chdir("/path/to/your/project")
+    os.chdir("E:\github_lhfim")
+    
+    os.system("git pull origin main")
+    
+
+ft.app(target=main)
